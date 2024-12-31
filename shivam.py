9ce4f0da5a6da584df1cc0968c99f7c96bd190ac
shisak55555 <drshivamsingh67@gmail.com>
2024-12-31 23:56:41 +0530

    Update shivam.py

--- shivam.py
+++ shivam.py
@@ -12,7 +12,7 @@ from pymongo import MongoClient
 from datetime import datetime, timedelta, timezone
 
 # Database Configuration
-MONGO_URI = 'mongodb+srv://Magic:Spike@cluster0.fa68l.mongodb.net/TEST?retryWrites=true&w=majority&appName=Cluster0'
+MONGO_URI = 'mongodb+srv://shivamx:shivamx@cluster0.mnnm3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 client = MongoClient(MONGO_URI)
 db = client['TEST']
 users_collection = db['users']
